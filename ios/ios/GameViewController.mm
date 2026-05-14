e924868873167a358171813a578cdca3cc904d6f
slavpoznyakow486-sketch <slavpoznyakow486@gmail.com>
2026-05-14 21:15:52 +0300

    Create GameViewController.mm

---
+++ ios/ios/GameViewController.mm
@@ -0,0 +1,15 @@
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+@end
+
+@implementation GameViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.blackColor;
+
+    NSLog(@"SAMP iOS started");
+}
+
+@end
